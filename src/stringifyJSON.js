b06e1b3b1941a00ebe834a85e4cc8d8b76f73972
nrobeR <mr.nrober@gmail.com>
2014-07-06 17:42:40 -0400

    stringify 0.1

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,16 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = [];
+  if(typeof(obj)==='string') return obj;
+  else if(typeof(obj)==='number') return obj.toString();
+  else if(obj === undefined) return 'null';
+  else{
+  	for(var key in obj){
+  		var keystring = '"' + key + '":';
+  		var valuestring = stringifyJSON(obj[key]);
+  		result.push(keystring + valuestring);
+  	}
+  	return "{" + result.join(",") + "}";
+  }
 };
