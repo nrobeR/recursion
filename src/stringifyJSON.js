4cdce526b73c698348e88aa260b85738462b23be
nrobeR <mr.nrober@gmail.com>
2014-07-21 15:42:00 -0700

    We're testing obj scenarios

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,23 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof(obj)==='object'){
+    var result = [];
+    for(var key in obj){
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + result + '}';
+  }
+  if (Array.isArray(obj)) {
+    // return '[' + stringifyJSON(obj) + ']';
+    var result = [];
+    for(var i = 0; i<obj.length;i++){
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  return ''  + obj;
 };
